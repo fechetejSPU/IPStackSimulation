1c1ee5168bcb32b2cb9be9141bcfee1b4aabc05f
scanlona <59579777+scanlona@users.noreply.github.com>
2025-05-23 14:46:01 -0700

    Adjust output with send and receive functions, testing with functions
    Added endl to the send and receive functions in application.cpp and transport.cpp to fix how it outputs, and tested the function with the commented out code below.

--- layers/application.cpp
+++ layers/application.cpp
@@ -5,9 +5,25 @@
 using namespace std;
 
 void sendApplicationLayer(string outputMessage) {
-    cout << "[Application Layer] Sending: " << outputMessage;
+    cout << "[Application Layer] Sending: " << outputMessage << endl;
 }
 
 void receiveApplicationLayer(string outputMessage) {
-    cout << "[Application Layer] Receiving: APP_HDR|" << outputMessage;
+    cout << "[Application Layer] Receiving: APP_HDR|" << outputMessage << endl;
 }
+
+/*
+Example of how program might run.
+
+int main() {
+    string myMessage = " ";
+    cout << "Starting program..." << endl << endl;
+    cout << "Enter your message: " << endl;
+    cin >> myMessage;
+    sendApplicationLayer(myMessage);
+    receiveApplicationLayer(myMessage);
+    cout << endl << "Program complete." << endl;
+    return 0;
+}
+
+*/
--- layers/transport.cpp
+++ layers/transport.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 void sendTransportLayer(string outputMessage) {
-    cout << "[Transport Layer] Sending: APP_HDR|" << outputMessage;
+    cout << "[Transport Layer] Sending: APP_HDR|" << outputMessage << endl;
 }
 
 void receiveTransportLayer(string outputMessage) {
-    cout << "[Transport Layer] Receiving: TRANS_HDR|APP_HDR|" << outputMessage;
+    cout << "[Transport Layer] Receiving: TRANS_HDR|APP_HDR|" << outputMessage << endl;
 }
