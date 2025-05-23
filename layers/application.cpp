df0f0ce863aeb8c60d1e293b07b766ea9f6c3929
scanlona <59579777+scanlona@users.noreply.github.com>
2025-05-23 14:27:44 -0700

    Transport and Application Layer - Establishing Functions, Formatting Desired Output
    Here, I am working with the transport and application layers for our assignment, creating functions for both sending and receiving on these layers, interacting with the sent message, but simulating the different parts of the IP stack process, as the message is handed off to different areas.

--- layers/application.cpp
+++ layers/application.cpp
@@ -1,1 +1,13 @@
 #include "application.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sendApplicationLayer(string outputMessage) {
+    cout << "[Application Layer] Sending: " << outputMessage;
+}
+
+void receiveApplicationLayer(string outputMessage) {
+    cout << "[Application Layer] Receiving: APP_HDR|" << outputMessage;
+}
--- layers/transport.cpp
+++ layers/transport.cpp
@@ -1,1 +1,13 @@
 #include "transport.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sendTransportLayer(string outputMessage) {
+    cout << "[Transport Layer] Sending: APP_HDR|" << outputMessage;
+}
+
+void receiveTransportLayer(string outputMessage) {
+    cout << "[Transport Layer] Receiving: TRANS_HDR|APP_HDR|" << outputMessage;
+}
